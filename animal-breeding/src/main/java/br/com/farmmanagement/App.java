4be6a93f2b21d5fcae8d5a898be561a470821094
Jemima Xavier <mimaxavier01@gmail.com>
2026-07-26 20:38:58 -0300

    Initial project structure

---
+++ animal-breeding/src/main/java/br/com/farmmanagement/App.java
@@ -0,0 +1,13 @@
+package br.com.farmmanagement;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
