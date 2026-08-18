cc270bacd688d01d54b9c3f15ada4f17603257f6
Jemima Xavier <mimaxavier01@gmail.com>
2026-08-18 07:34:33 -0300

    chore: configure Spring Boot and create breeding animal repository

---
+++ animal-breeding/src/main/java/br/com/farmmanagement/repository/BreedingAnimalRepository.java
@@ -0,0 +1,8 @@
+package br.com.farmmanagement.repository;
+
+import br.com.farmmanagement.model.BreedingAnimal;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface BreedingAnimalRepository extends JpaRepository<BreedingAnimal, Long> {
+
+}
